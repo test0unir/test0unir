7885b27c712d1e1fa349751aafe6f3519cd0f403
Jhostyn Benalcazar <jhostyn0305@hotmail.com>
2024-04-29 21:06:47 -0500

    Feature: Creación de la rama y actualización del método.

--- funcionalidad_jhostyn.py
+++ funcionalidad_jhostyn.py
@@ -1,1 +1,7 @@
-print("HOLA2!!")
+def contar_palabras(frase):
+    palabras = frase.split()
+    return len(palabras)
+
+frase = input("Ingresa una frase: ")
+numero_palabras = contar_palabras(frase)
+print("El número de palabras en la frase es:", numero_palabras)
